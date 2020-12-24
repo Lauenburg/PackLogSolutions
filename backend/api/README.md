4da3872a73cc74df7ecc00ac34a448afd162548f
Lukas Pries <lukas.pries@tum.de>
2020-12-24 15:06:51 +0100

    setup api with POST request

---
+++ backend/api/README.md
@@ -0,0 +1,13 @@
+# REST API
+
+## How to use API
+
+### Start server
+```
+python api.py
+```
+
+### Test with POST request
+```
+python test_api.py
+```
--- backend/api/api.py
+++ backend/api/api.py
@@ -1,13 +1,25 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 
+from ..logic import *
+# from ...data import DataManager
+
+import json
+
 app = Flask("PackLogAPI")
 api = Api(app)
 
+# data_manager = DataManager(args.password, args.dbname)
+
 class Estimator(Resource):
-    def get(self):
-        print(request.values)
-        return {'estimate': request.values}
+    def post(self):
+        # get data from post request
+        data = request.get_json()
+
+        # process data & get estimate
+        estimate = 0
+
+        return {'estimate': estimate}
 
 api.add_resource(Estimator, '/estimator')
 
